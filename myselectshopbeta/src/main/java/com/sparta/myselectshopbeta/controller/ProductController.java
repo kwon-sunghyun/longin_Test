4d528b7f5f6b0be82119ce102afc60c61b7ff039
sunghyun <116361833+kwon-sunghyun@users.noreply.github.com>
2022-12-04 22:26:56 +0900

    Add files via upload

---
+++ myselectshopbeta/src/main/java/com/sparta/myselectshopbeta/controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.sparta.myselectshopbeta.controller;
+
+import com.sparta.myselectshopbeta.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshopbeta.dto.ProductRequestDto;
+import com.sparta.myselectshopbeta.dto.ProductResponseDto;
+import com.sparta.myselectshopbeta.entity.Product;
+import com.sparta.myselectshopbeta.service.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    // 관심 상품 등록하기
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, HttpServletRequest request) {
+        // 응답 보내기
+        return productService.createProduct(requestDto, request);
+    }
+
+    // 관심 상품 조회하기
+    @GetMapping("/products")
+    public Page<Product> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            HttpServletRequest request
+    ) {
+        // 응답 보내기
+        return productService.getProducts(request, page-1, size, sortBy, isAsc);
+    }
+
+    // 관심 상품 최저가 등록하기
+    @PutMapping("/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto, HttpServletRequest request) {
+        // 응답 보내기 (업데이트된 상품 id)
+        return productService.updateProduct(id, requestDto, request);
+    }
+
+    // 상품에 폴더 추가
+    @PostMapping("/products/{productId}/folder")
+    public Long addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            HttpServletRequest request
+    ) {
+        Product product = productService.addFolder(productId, folderId, request);
+        return product.getId();
+    }
+
+}
---
+++ myselectshopbeta/src/main/java/com/sparta/myselectshopbeta/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.sparta.myselectshopbeta.controller;
+
+import com.sparta.myselectshopbeta.dto.LoginRequestDto;
+import com.sparta.myselectshopbeta.dto.SignupRequestDto;
+import com.sparta.myselectshopbeta.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public ModelAndView signupPage() {
+        return new ModelAndView("signup");
+    }
+
+    @GetMapping("/login")
+    public ModelAndView loginPage() {
+        return new ModelAndView("login");
+    }
+
+    @PostMapping("/signup")
+    public String signup(SignupRequestDto signupRequestDto) {
+        userService.signup(signupRequestDto);
+        return "redirect:/api/user/login";
+    }
+
+
+    @ResponseBody
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        userService.login(loginRequestDto, response);
+        return "success";
+    }
+
+}
